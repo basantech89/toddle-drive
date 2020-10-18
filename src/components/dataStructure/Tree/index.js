52272092e573a6b50474d62ca55e6d42249da02a
Basant Soni <basantech89@gmail.com>
2020-10-18 13:53:10 +0530

    refactor prototypal inheritance to classes

--- src/components/dataStructure/Tree/index.js
+++ src/components/dataStructure/Tree/index.js
@@ -8,71 +8,6 @@ export const Node = function (node) {
   this.children = []
 }
 
-export const Tree = function () {
-  this.root = new Node({
-    name: 'Home',
-    type: null,
-    id: v4(),
-    parent: null
-  })
-}
-
-Tree.prototype.getRootNode = function () {
-  return this.root
-}
-
-Tree.prototype.insert = function (newItem) {
-  let newNode
-  newNode = new Node(newItem)
-  if (!newItem.parent) {
-    newNode.parent = this.root
-    this.root.children.push(newNode)
-  } else {
-    let queue = [this.root]
-    let child
-    while (queue.length > 0) {
-      child = queue.shift()
-      if (child.id === newItem.parent.id) {
-        newNode.parent = child
-        child.children.push(newNode)
-        return
-      } else {
-        queue.push(...child.children)
-      }
-    }
-  }
-}
-
-Tree.prototype.rename = function (node, newName) {
-  let queue = [this.root]
-  let child
-  while (queue.length > 0) {
-    child = queue.shift()
-    if (child.id === node.id) {
-      child.name = newName
-      return ''
-    } else {
-      queue.push(...child.children)
-    }
-  }
-}
-
-Tree.prototype.remove = function (node) {
-  let queue = [this.root]
-  let child
-  while (queue.length > 0) {
-    child = queue.shift()
-    if (child.id === node.id) {
-      const parent = child.parent
-      const childIdx = parent.children.findIndex((item) => item.id === node.id)
-      parent.children.splice(childIdx, 1)
-      return
-    } else {
-      queue.push(...child.children)
-    }
-  }
-}
-
 function getNodeName(fromNode, toNode) {
   let newName = fromNode.name
   if (fromNode.parent === toNode) {
@@ -88,56 +23,125 @@ function getNodeName(fromNode, toNode) {
   return newName
 }
 
-Tree.prototype.copy = function (copiedNode, toNode) {
-  let queue = [this.root]
-  let child
-  while (queue.length > 0) {
-    child = queue.shift()
-    if (child.id === toNode.id) {
-      const pastedNode = new Node({
-        name: getNodeName(copiedNode, toNode),
-        type: copiedNode.type,
-        parent: child,
-        id: v4()
-      })
-      pastedNode.children = [...copiedNode.children]
-      child.children.push(pastedNode)
-      return
+export class Tree {
+  constructor() {
+    this.root = new Node({
+      name: 'Home',
+      type: null,
+      id: v4(),
+      parent: null
+    })
+  }
+
+  getRootNode() {
+    return this.root
+  }
+
+  insert(newItem) {
+    let newNode
+    newNode = new Node(newItem)
+    if (!newItem.parent) {
+      newNode.parent = this.root
+      this.root.children.push(newNode)
     } else {
-      queue.push(...child.children)
+      let queue = [this.root]
+      let child
+      while (queue.length > 0) {
+        child = queue.shift()
+        if (child.id === newItem.parent.id) {
+          newNode.parent = child
+          child.children.push(newNode)
+          return
+        } else {
+          queue.push(...child.children)
+        }
+      }
     }
   }
-}
 
-Tree.prototype.reorder = function (nodeID, reOrderedChildren) {
-  let queue = [this.root]
-  let child
-  while (queue.length > 0) {
-    child = queue.shift()
-    if (child.id === nodeID) {
-      child.children = [...reOrderedChildren]
-      return
-    } else {
-      queue.push(...child.children)
+  rename(node, newName) {
+    let queue = [this.root]
+    let child
+    while (queue.length > 0) {
+      child = queue.shift()
+      if (child.id === node.id) {
+        child.name = newName
+        return ''
+      } else {
+        queue.push(...child.children)
+      }
     }
   }
-}
 
-Tree.prototype.move = function (fromIdx, toID) {
-  let queue = [this.root]
-  let toNode
-  let child
-  while (queue.length > 0) {
-    child = queue.shift()
-    if (child.id === toID) {
-      toNode = child
-      const fromNode = toNode.parent.children.splice(fromIdx, 1)[0]
-      fromNode.name = getNodeName(fromNode, toNode)
-      fromNode.parent = toNode
-      toNode.children.push(fromNode)
-      return toNode.parent.children
-    } else {
-      queue.push(...child.children)
+  remove(node) {
+    let queue = [this.root]
+    let child
+    while (queue.length > 0) {
+      child = queue.shift()
+      if (child.id === node.id) {
+        const parent = child.parent
+        const childIdx = parent.children.findIndex(
+          (item) => item.id === node.id
+        )
+        parent.children.splice(childIdx, 1)
+        return
+      } else {
+        queue.push(...child.children)
+      }
+    }
+  }
+
+  copy(copiedNode, toNode) {
+    let queue = [this.root]
+    let child
+    while (queue.length > 0) {
+      child = queue.shift()
+      if (child.id === toNode.id) {
+        const pastedNode = new Node({
+          name: getNodeName(copiedNode, toNode),
+          type: copiedNode.type,
+          parent: child,
+          id: v4()
+        })
+        pastedNode.children = [...copiedNode.children]
+        child.children.push(pastedNode)
+        return
+      } else {
+        queue.push(...child.children)
+      }
+    }
+  }
+
+  reorder(nodeID, reOrderedChildren) {
+    let queue = [this.root]
+    let child
+    while (queue.length > 0) {
+      child = queue.shift()
+      if (child.id === nodeID) {
+        child.children = [...reOrderedChildren]
+        return
+      } else {
+        queue.push(...child.children)
+      }
+    }
+  }
+
+  move(fromIdx, toID) {
+    let queue = [this.root]
+    let toNode
+    let child
+    while (queue.length > 0) {
+      child = queue.shift()
+      if (child.id === toID) {
+        toNode = child
+        const fromNode = toNode.parent.children.splice(fromIdx, 1)[0]
+        fromNode.name = getNodeName(fromNode, toNode)
+        fromNode.parent = toNode
+        toNode.children.push(fromNode)
+        return toNode.parent.children
+      } else {
+        queue.push(...child.children)
+      }
     }
   }
 }
